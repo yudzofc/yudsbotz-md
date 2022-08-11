9d61a9aaf1b18602551f0498ebd0e3cb6e1b0dae
yudzofc <106936091+yudzofc@users.noreply.github.com>
2022-08-11 17:47:18 +0700

    apa nyett

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -11,7 +11,7 @@ let text = `
 ❏────
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: '✨ Saweria', url: psaweria}},
+    {index: 1, urlButton: {displayText: '✨ Sociabuzz', url: psaweria}},
     {index: 2, urlButton: {displayText: '📷 Instagram', url: sig}},
     {index: 3, urlButton: {displayText: '🌎 Official Group', url: sgc}},
     {index: 4, quickReplyButton: {displayText: 'Menu', id: '.menu'}},
